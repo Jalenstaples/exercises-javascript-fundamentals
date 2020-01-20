c8b55ddfb5a78abc71b2b4d05a134349baab58c2
Jalenstaples <59895948+Jalenstaples@users.noreply.github.com>
2020-01-19 22:51:39 -0800

    Update countGreaterThan.js

--- exercises/arrays/countGreaterThan/countGreaterThan.js
+++ exercises/arrays/countGreaterThan/countGreaterThan.js
@@ -15,13 +15,19 @@
  * @param {number} threshold - The threshold for counting
  * @returns {number} The number of elements in the array greater than threshold
  */
-function countGreaterThan(array, threshold) {
+function countGreaterThan(array, num) {
   // This is your job. :)
+  let count=0;
+for (let i of array){
+if (i>num){
+count+=1;}
+}
+return count;
 }
 
 if (require.main === module) {
   console.log('Running sanity checks for countGreaterThan:');
-
+console.log(countGreaterThan([6,7,8,9,10,11],9));
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
 }
