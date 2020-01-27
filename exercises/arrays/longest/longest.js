57f69fe5ccaa5486fc373ac85e3ef842b8d37134
Jalenstaples <59895948+Jalenstaples@users.noreply.github.com>
2020-01-27 15:23:21 -0800

    Update longest.js

--- exercises/arrays/longest/longest.js
+++ exercises/arrays/longest/longest.js
@@ -7,12 +7,17 @@
  * @returns {number} The longest string in the array
  */
 function longest(array) {
-  // This is your job. :)
+  let longest=array[0];
+  for (let i of array){
+  if (i.length>longest.length)
+  longest=i
+}
+return longest
 }
 
 if (require.main === module) {
   console.log('Running sanity checks for longest:');
-
+  console.log(longest(['hi','yes', 'cool']));
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
 }
