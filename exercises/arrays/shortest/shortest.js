46fba4f9daa9f541bf09420fb1319bf6a3ebc282
Jalenstaples <59895948+Jalenstaples@users.noreply.github.com>
2020-01-27 15:16:19 -0800

    Update shortest.js

--- exercises/arrays/shortest/shortest.js
+++ exercises/arrays/shortest/shortest.js
@@ -7,12 +7,16 @@
  * @returns {number} The shortest string in the array
  */
 function shortest(array) {
-  // This is your job. :)
+let shortest=array[0];
+for (let i of array){
+if (i.length<shortest.length)
+shotrtest=i
 }
-
+return shortest
+}
 if (require.main === module) {
   console.log('Running sanity checks for shortest:');
-
+console.log(shortest(['hi','yes', 'cool']));
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
 }
