eb21451c5ee3cdb467eeccc10b0dc2d58d31675e
Jalen Staples <jastaples@davidson.edu>
2020-02-06 15:06:37 -0800

    2/6 Project

--- exercises/branching-conditionals/rockPaperScissorsWinner/rockPaperScissorsWinner.js
+++ exercises/branching-conditionals/rockPaperScissorsWinner/rockPaperScissorsWinner.js
@@ -24,12 +24,53 @@
  * @returns {string} Returns 'win', 'lose', or 'draw' depending on
  *  whether the first player won, lost, or drew the match.
  */
+let readlineSync = require('readline-sync');
+console.log('Welcome to Rock, Paper, Scissors!');
+console.log('For selection: Rock = 2 | Paper = 1 | Scissors = 0');
+let user1 = readlineSync.question('What is your choice?');
+let user2 = Math.floor(Math.random() * 3);
+console.log(user2);
+let arr = [user1, user2];
+console.log(arr);
+console.log(rockPaperScissorsWinner(arr));
+
 function rockPaperScissorsWinner(array) {
-  // This is your job. :)
+  let reuslt1 = 'Congrats,You Win!';
+  let result2 = 'Sorry,You Lose!';
+  let result3 = 'Tie!';
+
+  if (array[0] === '0' && array[1] === 0) {
+    return result3;
+  }
+  if (array[0] === '0' && array[1] === 1) {
+    return reuslt1;
+  }
+  if (array[0] === '0' && array[1] === 2) {
+    return result2;
+  }
+
+  if (array[0] === '1' && array[1] === 0) {
+    return result2;
+  }
+  if (array[0] === '1' && array[1] === 1) {
+    return result3;
+  }
+  if (array[0] === '1' && array[1] === 2) {
+    return reuslt1;
+  }
+
+  if (array[0] === '2' && array[1] === 0) {
+    return reuslt1;
+  }
+  if (array[0] === '2' && array[1] === 1) {
+    return result2;
+  }
+  if (array[0] === '2' && array[1] === 2) {
+    return result3;
+  }
 }
 
 if (require.main === module) {
-  console.log('Running sanity checks for rockPaperScissorsWinner:');
 
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
